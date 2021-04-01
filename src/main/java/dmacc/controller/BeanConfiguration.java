3e45bbe60bdf0a0263ed8d9d354d711b4bddc971
Noah Hobbs <mlzgphoenix@gmail.com>
2021-04-01 15:02:17 -0500

    fourth commit adding the ability to add a game

--- src/main/java/dmacc/controller/BeanConfiguration.java
+++ src/main/java/dmacc/controller/BeanConfiguration.java
@@ -3,7 +3,18 @@ package dmacc.controller;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import dmacc.beans.VideoGames;
+
 @Configuration
 public class BeanConfiguration {
-
+	@Bean
+	public VideoGames VideoGames() {
+		VideoGames bean = new VideoGames("Eric Carle");
+		bean.setGameName("Fortnite");
+		bean.setYearOfRelease("2016");
+		bean.setPrice(19.99);
+		bean.setRating("T");
+		return bean;
+		
+	}
 }
--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import dmacc.beans.VideoGames;
 import dmacc.repository.GameRepository;
 
 @Controller
@@ -18,5 +21,17 @@ public class WebController {
 		
 		model.addAttribute("games",repo.findAll());
 		return "results";
+	}
+	
+	@GetMapping("/inputGame")
+	public String addNewGame(Model model) {
+		VideoGames v = new VideoGames();
+		model.addAttribute("newGame", v);
+		return "input";
+	}
+	@PostMapping("/inputGame")
+	public String addNewGame(@ModelAttribute VideoGames v, Model model) {
+		repo.save(v);
+		return viewAllGames(model);
 	}
 }
